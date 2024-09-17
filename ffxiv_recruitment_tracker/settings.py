7355f4a1135c091c90d07de7dcd604b446954988
MellGriffin <missmelissagriffin@gmail.com>
2024-09-17 17:11:43 -0500

    Created auth functionality

--- ffxiv_recruitment_tracker/settings.py
+++ ffxiv_recruitment_tracker/settings.py
@@ -118,6 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_URL = 'home'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'about'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
